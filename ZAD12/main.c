2a183111c3fa9e26fc9d4382534ad71526d026e1
BlaSee01 <92850744+BlaSee01@users.noreply.github.com>
2021-11-05 10:53:33 +0100

    ZAD12

--- ZAD12/main.c
+++ ZAD12/main.c
@@ -10,12 +10,25 @@ int main()
 
     int b = n;
 
+    if(n>0){
     if(n == b){
         printf("Sufit: %d\n" , b);
         printf("Podloga: %d" , b);
     }else{
         printf("Sufit: %d\n" , b+1);
         printf("Podloga: %d" , b);
+    }
+    }else if(n<0){
+    if(n == b){
+        printf("Sufit: %d\n" , b);
+        printf("Podloga: %d" , b);
+    }else{
+        printf("Sufit: %d\n" , b);
+        printf("Podloga: %d" , b-1);
+    }
+    }else{
+        printf("Sufit: %d\n" , 0);
+        printf("Podloga: %d" , 0);
     }
 
     return 0;
